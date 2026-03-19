01f0c8facb0fb897fc2cdfa5d3a445162e95cd2f
Teamz Lab LTD <teamz.lab.contact@gmail.com>
2026-03-20 03:49:54 +0600

    Commit 2 more untracked tools

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@
  * - AdSense/Analytics: always skip (never cache)
  */
 
-var CACHE_NAME = 'teamztools-202603200349';
+var CACHE_NAME = 'teamztools-202603200350';
 var PRECACHE_URLS = [
   '/',
   '/branding/css/teamz-branding.css',
