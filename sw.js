b53995b8c4d67ca4b7543aac6e84612b5050112a
Teamz Lab LTD <teamz.lab.contact@gmail.com>
2026-03-27 21:54:26 +0600

    Auto maintenance: schemas, search index, SEO fixes

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@
  * - AdSense/Analytics: always skip (never cache)
  */
 
-var CACHE_NAME = 'teamztools-202603272151';
+var CACHE_NAME = 'teamztools-202603272154';
 var PRECACHE_URLS = [
   '/',
   '/branding/css/teamz-branding.css',
