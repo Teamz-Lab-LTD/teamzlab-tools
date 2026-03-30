7b3244f21224977998f79f473dcca6ceacff1f4b
Teamz Lab LTD <teamz.lab.contact@gmail.com>
2026-03-30 18:46:48 +0600

    Sync llms metadata for 189 title

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@
  * - AdSense/Analytics: always skip (never cache)
  */
 
-var CACHE_NAME = 'teamztools-202603301846';
+var CACHE_NAME = 'teamztools-202603301847';
 var PRECACHE_URLS = [
   '/',
   '/branding/css/teamz-branding.css',
