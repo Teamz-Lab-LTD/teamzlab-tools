f06599e826f461b1fe11210df58f965a4f9cf4cb
GK emon <emon@getonnet.agency>
2026-04-07 09:43:45 +0600

    Auto maintenance: schemas, search index, SEO fixes

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@
  * - AdSense/Analytics: always skip (never cache)
  */
 
-var CACHE_NAME = 'teamztools-202604070935';
+var CACHE_NAME = 'teamztools-202604070944';
 var PRECACHE_URLS = [
   '/',
   '/branding/css/teamz-branding.css',
