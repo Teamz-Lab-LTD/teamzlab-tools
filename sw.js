08ccae5189b7eb80cf7fb6dcfa1af96e4213319a
GK emon <emon@getonnet.agency>
2026-04-17 02:50:03 +0600

    i18n: 5 career tools

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@
  * - AdSense/Analytics: always skip (never cache)
  */
 
-var CACHE_NAME = 'teamztools-202604170222';
+var CACHE_NAME = 'teamztools-202604170251';
 var PRECACHE_URLS = [
   '/',
   '/branding/css/teamz-branding.css',
